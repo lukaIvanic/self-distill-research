6fd3d37d4a161f5ab0412c34ea4a0b5b3eb0b02b
lukaIvanic7 <luka.ivanic7@gmail.com>
2025-07-11 19:30:08 +0200

    Moj update denis

--- main_space/settings.py
+++ main_space/settings.py
@@ -69,11 +69,11 @@ class TrainingConfig:
 
         def __init__(self):
             self.vocab_size = 5000  # Dummy vocab size
-            self.d_model = 32  # Embedding dimension / model dimension
-            self.num_heads = 1  # Number of attention heads
-            self.num_layers = 6  # Number of Transformer blocks
-            self.ctx_len = 64  # Max sequence length for dummy data and positional embeddings
-            self.dropout_rate = 0.0
+            self.d_model = 256  # Embedding dimension / model dimension
+            self.num_heads = 4  # Number of attention heads
+            self.num_layers = 5  # Number of Transformer blocks
+            self.ctx_len = 512  # Max sequence length for dummy data and positional embeddings
+            self.dropout_rate = 0.1
 
     class DistillConfig:
 
@@ -86,7 +86,7 @@ class TrainingConfig:
         self.warmup_steps = int(1e3)
         self.train_steps = int(1e4)
         self.peak_lr = 1e-3
-        self.batch_size = 64
+        self.batch_size = 32
         self.scheduler_type = "cosine"  # Options: "cosine", "inverse_sqrt", "linear"
         self.min_lr = 1e-4
         self.training_precision = "bfloat16"  # Options: "bfloat16", "float16" (uses GradScaler), "float32"
@@ -103,7 +103,7 @@ class TrainingConfig:
         self.scaler = None
         self.initialize_precision()
         self.doesClipGradients = True
-        self.distill_enabled = True
+        self.distill_enabled = False
 
         self.hyperParamConfig = self.HyperparameterConfig()
         self.distillConfig = self.DistillConfig()
